751d1c62f7ccb78ef5422da16da87a704d07656d
Shirley <shirleyzhu@college.harvard.edu>
2024-01-03 22:09:52 -0600

    add array of emails

--- login.js
+++ login.js
@@ -1,8 +1,41 @@
+const email_array = ["meiyiyan@college.harvard.edu",
+    "melissa_liu@college.harvard.edu",
+    "adriennechan@college.harvard.edu",
+    "allychun@college.harvard.edu",
+    "Astickels@college.Harvard.edu",
+    "asong@college.harvard.edu",
+    "ashleykim@collegeharvard.edu",
+    "cskim@college.harvard.edu",
+    "hannahgokongwei@college.harvard.edu",
+    "jamgalanbaatar@college.harvard.edu",
+    "jisunglee@college.harvard.edu",
+    "kelseywu@college.harvard.edu",
+    "elizabethli@college.harvard.edu",
+    "michellesun@college.harvard.edu",
+    "scai@college.harvard.edu",
+    "tahuja@college.Harvard.edu",
+    "althealee@college.harvard.edu",
+    "chelseawang@college.harvard.edu",
+    "Epham@college.harvard.edu",
+    "emily_chan@college.harvard.edu",
+    "jgurm@college.harvard.edu",
+    "jenny_lu@college.harvard.edu",
+    "jodie_kuo@college.harvard.edu",
+    "joyho@college.harvard.edu",
+    "jooeunjunelee@college.harvard.edu",
+    "katielim@college.harvard.edu",
+    "lxmai@college.harvard.edu",
+    "michellelin@college.harvard.edu",
+    "nicolelu@college.harvard.edu",
+    "shirleyzhu@college.harvard.edu",
+    "yukichen@college.harvard.edu",
+    "yjhan@college.harvard.edu",
+    "amyliu@college.harvard.edu"]
 function auth() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    if (email === "shirleyzhu@college.harvard.edu" && password === "purpleorchid123!") {
+    if (email_array.includes(email) && password === "purpleorchid123!") {
         window.location.href = "https://harvardoaasis.com/oasis";
     } else {
         alert("Invalid information");
